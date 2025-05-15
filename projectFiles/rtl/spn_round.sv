5560a243240982b37101b604e92cd5e9b8f0fed2
LaithSalahAldin <laithahmadshaba7@gmail.com>
2025-05-16 01:46:15 +0300

    Renamed Variables

--- projectFiles/rtl/spn_round.sv
+++ projectFiles/rtl/spn_round.sv
@@ -4,17 +4,17 @@
 `include "spn_sbox_pkg.sv"
 module spn_round #(int DW = 16)(
   input  logic [DW-1:0] data_in,
-  input  logic [DW-1:0] key_mix,   // 16-bit round key
+  input  logic [DW-1:0] round_key,
   output logic [DW-1:0] data_out
 );
   import spn_sbox_pkg::*;
 
-  // 1️⃣ Key-mix (XOR)
-  logic [DW-1:0] after_xor = data_in ^ key_mix;
+  // Key-mix (XOR)
+  logic [DW-1:0] after_xor = data_in ^ round_key;
 
-  // 2️⃣ Substitution (4 parallel 4-bit S-boxes)
+  // Substitution Layer (S-box) (4 parallel 4-bit S-boxes)
   logic [DW-1:0] after_s   = sbox_substitute(after_xor);
 
-  // 3️⃣ Permutation — rotate left by one byte (8 bits)
+  // Permutation Layer (P-box) — rotate left by one byte (8 bits)
   assign data_out = {after_s[7:0], after_s[15:8]};
 endmodule
