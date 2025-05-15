92c6b7aa46984343ae26b0eeba95b9094e913a77
LaithSalahAldin <laithahmadshaba7@gmail.com>
2025-05-16 01:45:55 +0300

    Renamed Variables

--- projectFiles/rtl/spn_if.sv
+++ projectFiles/rtl/spn_if.sv
@@ -3,20 +3,21 @@
 // ============================================================================
 interface spn_if #(int DW = 16, KW = 32) (input logic clk, rst_n);
 
-  /*-------------- Inputs driven to the DUT -----------------------------*/
-  logic [1:0]  opcode;    // 00=nop 01=enc 10=dec
-  logic [DW-1:0] data_i;  // plaintext / ciphertext
-  logic [KW-1:0] key_i;   // 32-bit secret key
+  // Inputs driven to the DUT
+  logic [1:0]    opcode;    // 00=nop 01=enc 10=dec 11=undefined
+  logic [DW-1:0] data_in;  // plaintext / ciphertext
+  logic [KW-1:0] symmetric_secret_key;   // 32-bit secret key
 
-  /*-------------- Outputs driven from the DUT --------------------------*/
-  logic [DW-1:0] data_o;  // result
-  logic [1:0]    valid;   // echoes opcode when done (01/10)
+  // Outputs driven from the DUT
+  logic [DW-1:0] data_out;  // result
+  logic [1:0]    valid;     // 00: no valid output, 01: successful encryption, 10: successful decryption  
+                            // 11: internal error or undefined operation
 
   /*-------------- Direction-controlled modports ---------------------------*/
-  modport dut (input  clk, rst_n, opcode, data_i, key_i,
-               output data_o, valid);
+  modport dut (input  clk, rst_n, opcode, data_in, symmetric_secret_key,
+               output data_out, valid);
                
-  modport tb  (output opcode, data_i, key_i,
-               input  clk, rst_n, data_o, valid);
+  modport tb  (output opcode, data_in, symmetric_secret_key,
+               input  clk, rst_n, data_out, valid);
 
 endinterface
