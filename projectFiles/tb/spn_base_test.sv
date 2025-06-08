d9f6dbeaea3500b512d343cf1a016c5757d258f1
LaithSalahAldin <laithahmadshaba7@gmail.com>
2025-06-08 08:25:43 +0300

    Cleaned and Added Comments

--- projectFiles/tb/spn_base_test.sv
+++ projectFiles/tb/spn_base_test.sv
@@ -8,22 +8,29 @@ class spn_base_test extends uvm_test;
     super.new(name,parent);
   endfunction : new
 
+  // ============================================================
+
   virtual function void build_phase(uvm_phase phase);
     super.build_phase(phase);
-    // Create the env
-    env = spn_model_env::type_id::create("env", this);    // Get the virtual interface from the config DB
+    // Create the envrionment
+    env = spn_model_env::type_id::create("env", this);    
+    // Get the virtual interface from the config DB
     if (!uvm_config_db#(virtual spn_if)::get(this, "", "vif", vif))
       `uvm_fatal("TEST", "Did not get vif")
-    
-      uvm_config_db#(virtual spn_if)::set(this, "*", "vif", vif);
+    // Set the virtual interface in the config DB for other components
+    uvm_config_db#(virtual spn_if)::set(this, "*", "vif", vif);
   endfunction : build_phase
-    virtual function void end_of_elaboration_phase(uvm_phase phase);
+
+  // ============================================================
+
+  virtual function void end_of_elaboration_phase(uvm_phase phase);
     super.end_of_elaboration_phase(phase);
     //print's the topology
     uvm_top.print_topology();
-  endfunction
+  endfunction : end_of_elaboration_phase
 
-  
+  // ============================================================
+
   function void report_phase(uvm_phase phase);
     uvm_report_server svr;
     
@@ -39,6 +46,6 @@ class spn_base_test extends uvm_test;
       `uvm_info(get_type_name(), "---------------------------------------", UVM_NONE)
     end
 
-  endfunction 
+  endfunction : report_phase
 
 endclass :spn_base_test
