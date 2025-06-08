4546a7e1676d1b363736516e235923bc9a0e2751
LaithSalahAldin <laithahmadshaba7@gmail.com>
2025-06-08 08:31:31 +0300

    Added the config DB parameters used by sequences

--- projectFiles/tb/testbench.sv
+++ projectFiles/tb/testbench.sv
@@ -26,6 +26,12 @@ module testbench;
 
   initial begin 
     uvm_config_db#(virtual spn_if)::set(uvm_root::get(), "*", "vif", spn_interface);
+    // uvm_config_db#(string)::set(null, "*", "sequence_type", "spn_sequence_combination");
+    // uvm_config_db#(int)::set(null, "*", "num_iterations", 15);
+    uvm_config_db#(int)::set(null, "*", "encrypt_transactions", 10);
+    uvm_config_db#(int)::set(null, "*", "decrypt_transactions", 8);
+    uvm_config_db#(int)::set(null, "*", "noop_transactions", 3);
+    uvm_config_db#(int)::set(null, "*", "undefined_transactions", 2);
     //enable wave dump
     $dumpfile("dump.vcd"); 
     $dumpvars;
