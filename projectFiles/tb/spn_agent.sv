9e5567c5fb10e4acc8c43d18e3d721ccbec2d9d0
LaithSalahAldin <laithahmadshaba7@gmail.com>
2025-06-08 08:25:09 +0300

    Cleaned

--- projectFiles/tb/spn_agent.sv
+++ projectFiles/tb/spn_agent.sv
@@ -1,28 +1,29 @@
 class spn_agent extends uvm_agent;
+  `uvm_component_utils(spn_agent)
 
   spn_driver    driver;
   spn_sequencer sequencer;
   spn_monitor   monitor;
 
-  `uvm_component_utils(spn_agent)
-
   function new (string name = "agent", uvm_component parent = null);
     super.new(name, parent);
   endfunction : new
 
+  // ========================================================================
 
   function void build_phase(uvm_phase phase);
     super.build_phase(phase);
     // Create the monitor
     monitor = spn_monitor::type_id::create("monitor", this);
 
-    //creating driver and sequencer only for ACTIVE agent
+    //Create the Driver and Sequencer Only if the agent is ACTIVE
     if(get_is_active() == UVM_ACTIVE) begin
       driver    = spn_driver::type_id::create("driver", this);
       sequencer = spn_sequencer::type_id::create("sequencer", this);
     end
   endfunction : build_phase
   
+  // ========================================================================
 
   function void connect_phase(uvm_phase phase);
     super.connect_phase(phase);
